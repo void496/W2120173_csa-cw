82755138a54b2f1d037222f42443d839a00f9cc3
void496 <anjana32123@gmail.com>
2026-04-23 07:54:49 +0530

    Initial JAX-RS project setup

---
+++ src/main/java/com/smartcampus/config/SmartCampusApplication.java
@@ -0,0 +1,8 @@
+package com.smartcampus.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+}
